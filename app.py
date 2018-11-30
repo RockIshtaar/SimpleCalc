f1d40518bed1545db0dba7da79a6ca4889589f52
rakeshguha15 <rakeshguha15@gmail.com>
2018-11-30 10:28:20 +0000

    Third Commit

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 ###Taking Input
 exp= raw_input("Please enter the expression ")
-res= eval(exp+'.0')
-print "The result is "+str(res)
+try :
+	res= eval(exp+'.0')
+	print "The result is "=str(res)
+except SyntaxError:
+	print "Error: Invalid Expression"
+
 
